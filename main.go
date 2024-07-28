eea0627d28a965b51209e1d6e74c9c14e90b70e8
yukimingo <hakamori5junk.1@gmail.com>
2024-07-28 17:37:07 +0900

    main()作成

--- main.go
+++ main.go
@@ -1,1 +1,32 @@
 package main
+
+import (
+	"good-and-new/infra"
+	"good-and-new/models"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	infra.Initialize()
+	db := infra.SetupDB()
+	// authRepository := repositories.NewAuthRepository(db)
+	u := &models.User{
+		Name:     "test",
+		Email:    "test@example.com",
+		Password: "password",
+	}
+
+	r := gin.Default()
+	r.POST("/users", func(ctx *gin.Context) {
+		if err := db.Create(u).Error; err != nil {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
+			return
+		}
+
+		ctx.Status(http.StatusOK)
+	})
+
+	r.Run("localhost:8080")
+}
