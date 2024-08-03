7bc3481cc810ee81ced4ac2cd9c7bdd0a44c0e0a
yukimingo <hakamori5junk.1@gmail.com>
2024-08-03 23:48:25 +0900

    不要なコードを削除

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"good-and-new/controllers"
 	"good-and-new/infra"
 	"good-and-new/models"
+	"good-and-new/repositories"
+	"good-and-new/usecases"
 	"net/http"
 	"strconv"
 
@@ -12,73 +15,27 @@ import (
 func main() {
 	infra.Initialize()
 	db := infra.SetupDB()
-	// authRepository := repositories.NewAuthRepository(db)
-	u := &models.User{
-		Name:     "test3",
-		Email:    "test3@example.com",
-		Password: "password3",
-	}
+	authRepository := repositories.NewAuthRepository(db)
+	authUsecase := usecases.NewAuthUsecase(authRepository)
+	authController := controllers.NewAuthController(authUsecase)
+
 	n := &models.News{
 		Title:       "title2",
 		Description: "これはテスト2です",
 		UserID:      7,
 	}
-	// updatedUser := &models.User{
-	// 	Name:     "updated user",
-	// 	Email:    "updated@example.com",
-	// 	Password: "updatedPassword",
-	// }
 
 	r := gin.Default()
-	r.POST("/users", func(ctx *gin.Context) {
-		if err := db.Create(u).Error; err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create user"})
-			return
-		}
+	r.POST("/users", authController.CreateUser)
 
-		ctx.Status(http.StatusOK)
-	})
-	r.GET("/users", func(ctx *gin.Context) {
-		var users []models.User
-		if err := db.Find(&users).Error; err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected error"})
-			return
-		}
+	r.GET("/users", authController.FindAll)
 
-		ctx.JSON(http.StatusOK, gin.H{"data": &users})
-	})
-	r.GET("/users/:id", func(ctx *gin.Context) {
-		var user models.User
-		userId, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
-			return
-		}
-		if err := db.First(&user, "id = ?", userId).Error; err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected error"})
-			return
-		}
+	r.GET("/users/:email", authController.FindUser)
 
-		ctx.JSON(http.StatusOK, gin.H{"data": &user})
-	})
-	r.DELETE("/users/:id", func(ctx *gin.Context) {
-		var user models.User
-		userId, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
-		if err != nil {
-			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Invalid id"})
-			return
-		}
-		if err := db.First(&user, "id = ?", userId).Error; err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected error"})
-			return
-		}
-		if err := db.Delete(&user).Error; err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "Unexpected error"})
-			return
-		}
+	r.GET("/user/:id", authController.FindUserById)
 
-		ctx.Status(http.StatusOK)
-	})
+	r.DELETE("/users/:id", authController.DeleteUser)
+
 	r.PUT("/users/:id", func(ctx *gin.Context) {
 		var user models.User
 		userId, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
