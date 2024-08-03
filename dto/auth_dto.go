84fbaf8c1273118829459e3d35c25785608795de
yukimingo <hakamori5junk.1@gmail.com>
2024-08-03 23:40:17 +0900

    dto/auth_dto.go作成

---
+++ dto/auth_dto.go
@@ -0,0 +1,7 @@
+package dto
+
+type SignupInput struct {
+	Name     string `json:"name" binding:"required,max=50"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,min=5"`
+}
